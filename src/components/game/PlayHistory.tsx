27edde61056e0cd12e61d1df261dca29b6986b20
u32634 <35775770+u32634@users.noreply.github.com>
2025-09-22 18:09:07 +0200

    Updated PlayHistory.tsx

--- src/components/game/PlayHistory.tsx
+++ src/components/game/PlayHistory.tsx
@@ -191,7 +191,7 @@ export default function PlayHistory({
 
   const containerClass = hideDelete
     ? ''
-    : 'bg-white dark:bg-gray-800 rounded-xl shadow-lg p-3 h-[calc(100vh-8rem)]';
+    : 'bg-white dark:bg-gray-800 rounded-xl shadow-lg p-3 h-[calc(100vh-9rem)]';
   
   return (
     <>
