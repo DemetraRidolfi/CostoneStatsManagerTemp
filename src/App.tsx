54e7db55188cd2f36ea24fde0475dfb3122610f8
u32634 <35775770+u32634@users.noreply.github.com>
2025-09-22 17:56:32 +0200

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import PastGames from './components/PastGames';
 import Statistics from './components/Statistics';
 import TeamManagement from './components/TeamManagement';
 import Settings from './components/Settings';
-import PWAInstallPrompt from './components/PWAInstallPrompt';
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const { isAuthenticated } = useAuth();
@@ -50,7 +49,6 @@ function AppContent() {
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
-          <PWAInstallPrompt />
         </NavigationProvider>
       ) : (
         <Routes>
